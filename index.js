e3212e43aeeb492b40090f6e33016e10783fb6a8
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-02-06 19:32:11 +0300

    add express async errors

--- index.js
+++ index.js
@@ -2,16 +2,25 @@
 const jobs = require('./routes/jobs');
 const recruiters = require('./routes/recruiters');
 const auth = require('./routes/auth');
+const error = require('./middleware/error');
+const config = require('config');
+const winston = require('winston');
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
+require('express-async-errors');
 const express = require('express');
 const app = express();
 
-app.use(express.json());
 
+if(!config.get("jwtPrivateKey"))
+      throw new Error('FATAL ERROR: jwtPrivateKey is not defined');
+
+
+app.use(express.json());
 app.use('/api/jobs', jobs);
 app.use('/api/recruiters', recruiters);
 app.use('/api/auth', auth);
+app.use(error);
 
 
 mongoose.connect('mongodb://localhost/jtank')
@@ -19,6 +28,6 @@ mongoose.connect('mongodb://localhost/jtank')
         .catch((err) => console.error('Connection unsuccesful ' + err));
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, console.log("listening on port " + port));
+const server = app.listen(port, () => winston.info(`listening on port ${port}`));
 
 module.exports = server;
